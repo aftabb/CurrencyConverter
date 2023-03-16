a49ca022b0e77bf0f029e37d50c11f37d7255f73
Aftab <aftab.bhadki@ergos.in>
2023-03-16 23:03:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/currencyconverter/data/server/CurrencyDbService.kt
@@ -0,0 +1,15 @@
+package com.example.currencyconverter.data.server
+
+import kotlinx.coroutines.Deferred
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CurrencyDbService {
+
+    @GET("latest")
+    fun getAllCurrencies(
+        @Query("access_key") key: String,
+        @Query("base") base: String,
+        @Query("symbols") symbols: String
+    ): Deferred<Currency>
+}
---
+++ app/src/main/java/com/example/currencyconverter/ui/App.kt
@@ -0,0 +1,21 @@
+package com.example.currencyconverter.ui
+
+import android.app.Application
+import com.example.currencyconverter.R
+import com.example.currencyconverter.di.initDI
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+//        CalligraphyConfig.initDefault(
+//            CalligraphyConfig.Builder()
+//                .setDefaultFontPath("fonts/Montserrat-Bold.ttf")
+//                .setFontAttrId(R.attr.fontPath)
+//                .build()
+    //    )
+
+        initDI()
+    }
+}
